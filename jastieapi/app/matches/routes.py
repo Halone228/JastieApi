989ca803548a79b1ec7a66070ac2ab0817b16df7
halone <haloneprog@gmail.com>
2024-03-11 16:01:01 +0300

    [-] Match add event

--- jastieapi/app/matches/routes.py
+++ jastieapi/app/matches/routes.py
@@ -83,9 +83,27 @@ async def create_bid(
 @matches_router.post('/match/create')
 async def create_match(
     match: MatchCreate,
-    matches_db_helper: Annotated[MatchesDBHelper, Depends(get_helper(MatchesDBHelper))]
+    matches_db_helper: matches_db_typevar,
+    users_db_helper: users_db_typevar
 ):
     await matches_db_helper.create_match(match)
+    await gather(
+        *
+        [
+            BotMethods.bot_client.send_message(
+                i,
+                f"Ставки на матч {match.first_opponent} vs {match.second_opponent}\n"
+                "Добавили в @JastieShop_bot, залетай и ставь баллы!\n\n"
+                "🔔Как заработать баллы:\n"
+                "1. Ставить ставки\n"
+                "2. Общаться в чате ( за каждое сообщение даются баллы)\n"
+                "3. Приводить рефералов\n\n"
+                "📌Что можно делать с баллами:\n"
+                "1. Покупать скины\n"
+                "2. Покупать VIP"
+            ) for i in await users_db_helper.get_all_users_ids()
+        ]
+    )
 
 
 @matches_router.get('/match/{match_id}/win/{first_team}')
@@ -103,24 +121,19 @@ async def set_win(
     ))
     match = await matches_db_helper.get_match(match_id)
     coff = match.first_coff if first_team else match.second_coff
-    points = {bid.user_id: bid.bid*coff for bid in bids if bid.first_select == first_team}
+    points = {bid.user_id: bid.bid * coff for bid in bids if bid.first_select == first_team}
     tasks.append(user_db_helper.add_points_bulk(
         ids_values=points,
         by='bid_result'
     ))
     users_message: dict = {
         bid.user_id:
-        f'Матч {match.match_name}.\n'
-        f'Победа за {match.first_opponent if first_team else match.second_opponent}\n'
-        f'Ваша ставка ({bid.bid:.2f}) была на {match.first_opponent if bid.first_select else match.second_opponent}'
+            f'Матч {match.match_name}.\n'
+            f'Победа за {match.first_opponent if first_team else match.second_opponent}\n'
+            f'Ваша ставка ({bid.bid:.2f}) была на {match.first_opponent if bid.first_select else match.second_opponent}'
         for bid in bids
     }
     tasks.append(
         send_messages(users_message)
     )
     await gather(*tasks)
-
-
-
-
-
